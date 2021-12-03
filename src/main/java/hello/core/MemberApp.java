0749602cf2604a76abaf5e2a89fe83181db362d3
JHYUN <jhyun3315@naver.com>
2021-12-03 14:25:32 +0900

    Initial commit

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.Member;
+
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findByMember(1L);
+        System.out.println("new member = " + member);
+        System.out.println("find Member = " + findMember.getName());
+
+    }
+}
